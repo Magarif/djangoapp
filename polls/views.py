b28c3077b04042509dbda389cd5d47147fa81282
Petar Popovic <petar.popovic@icloud.com>
2020-03-03 08:37:30 +0100

    [TASK] Define detail html template and update detail action logic

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
-from django.http import HttpResponse
 from .models import Question
 
 
@@ -12,7 +12,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s. " % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
